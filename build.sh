1015e0bd1285e5ff7ecdef542328ec32f14bc952
Nong Li <nongli@MacBook-Air-5.local>
2025-11-29 10:32:52 -0800

    Why

--- build.sh
+++ build.sh
@@ -9,3 +9,10 @@ echo "Building lib2..."
 pushd lib2
 cargo build
 popd
+
+# This works
+echo "Linking just lib 1..."
+g++ main.cc -Llib1/target/debug -llib1 -o out-lib1-only
+
+# Why does this work?
+g++ main.cc -Llib1/target/debug -Llib2/target/debug -llib1 -llib2  -o out-both
